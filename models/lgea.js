1b1ce271763ba0ce287e9b508602af2b9313e2c2
Abdullah <abdullahyahaya46@gmail.com>
2022-04-09 13:05:14 +0100

    Changed the Model Name

--- models/lgea.js
+++ models/lgea.js
@@ -14,4 +14,4 @@ const localGovSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('localGovernment', localGovSchema);
+module.exports = mongoose.model('lgea', localGovSchema);
