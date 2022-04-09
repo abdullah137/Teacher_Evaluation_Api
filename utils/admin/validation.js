d86e3668c84b5f863fb4059809074f9d6b9b9e74
Abdullah <abdullahyahaya46@gmail.com>
2022-04-09 13:11:10 +0100

    Added Inspector Validation Schema and Removed some bugs

--- utils/admin/validation.js
+++ utils/admin/validation.js
@@ -4,13 +4,12 @@ const Joi = require('joi');
 const registerValidation = data => {
 
     const schema = Joi.object({
-        firtName: Joi.string().min(6).required(),
+        firstName: Joi.string().min(6).required(),
         lastName: Joi.string().min(6).required(),
         email: Joi.string().required().email(),
         isAdmin: Joi.boolean().required(),
         profileImg: Joi.string(),
         password: Joi.string().min(6).required(),
-        otpCode: Joi.number().min(6).required(),
         isVerified: Joi.boolean()
     });
 
@@ -28,4 +27,36 @@ const loginValidation = data => {
     return schema.validate(data);
 }
 
-module.exports = { registerValidation, loginValidation }
+// Lgea Validation
+const lgeaValidation = data => {
+
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        color: Joi.string().required(),
+        image: Joi.string()
+    })
+
+    return schema.validate(data)
+}
+
+// Inspector Validatieon
+const inspectorValidation = data => {
+
+    const schema = Joi.object({
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        email: Joi.string().required().email(),
+        phone: Joi.string(),
+        schools: Joi.array().required(),
+        password: Joi.string().required()
+    })
+
+    return schema.validate(data);
+}
+
+module.exports = { 
+    registerValidation,
+    loginValidation,
+    lgeaValidation,
+    inspectorValidation
+}
