3fbb387ae59986332589443c64a416a92ca3f97d
Abdullah <abdullahyahaya46@gmail.com>
2022-04-10 10:01:49 +0100

    Added Some Other Properties

--- models/lgea.js
+++ models/lgea.js
@@ -11,7 +11,20 @@ const localGovSchema = new mongoose.Schema({
     },
     image: {
         type: String
-    }
+    },
+    otherInfo: [
+        { 
+            teachers: {
+                type: Array
+            },
+            schools: {
+                type: Array
+            },
+            inspectors: {
+                type: Array
+            }
+        }
+    ]
 });
 
 module.exports = mongoose.model('lgea', localGovSchema);
