b9dd96c6f912cfeeb24ba17887bb8b8c1122896b
Abdullah <abdullahyahaya46@gmail.com>
2022-05-07 22:42:09 +0100

    Refactored the codebase on jwtAuth

--- middleware/jwtAuth.js
+++ middleware/jwtAuth.js
@@ -1,24 +1,37 @@
 const jwt = require('jsonwebtoken');
+const Admin = require('../models/admin');
 
-function auth(req, res, next) {
-    const token = req.header('beep-auth-token');
+async function auth(req, res, next) {
+
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            // Get token from header and splitting to get token
+            token = req.headers.authorization.split(' ')[1];
+           
+            // verify token
+            const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+           
+            // get user from the token
+            req.admin = await Admin.findById(decoded.id).select('-password');
+            next()
+        }catch(error) {
+
+            res.status(401).json({
+                error: "NOT_AUTHORIZED",
+                status: false,
+                message: "Sorry, You are not authorized" 
+            });
+            return;
+        }
+    }
+
     if(!token) {
         res.status(401).json({
             error: "TOKEN_REQUIRED",
             status: false,
             message: "Sorry, Kindly Input Your Token"
-        });
-        return;
-    }
-
-    try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        req.admin = verified;
-    }   catch(error) {
-         res.status(400).json({
-            error: "INVALID_TOKEN",
-            status: true,
-            message: "Na wa for you oo" 
         });
         return;
     }
