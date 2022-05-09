247fdc5f9f04b3caf70497f86cf471308c9416e2
Abdullah <abdullahyahaya46@gmail.com>
2022-05-09 22:08:57 +0100

    Adding Protected Routes

--- routes/admin/teacher/index.js
+++ routes/admin/teacher/index.js
@@ -4,11 +4,14 @@ const router = express.Router();
 // importing the controller needed
 const { _getAll, _getSpecific, _insert, _update, _delete } = require('../../../controllers/admin/teacher/index');
 
+// Importing the authentication needed
+const { auth } = require('../../../middleware/jwtAuth');
+
 router.get('/', _getAll);
 
 router.get('/:id', _getSpecific);
 
-router.post('/', _insert);
+router.post('/', auth, _insert);
 
 router.put('/:id', _update);
 
