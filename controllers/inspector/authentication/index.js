148beecb705525229261d3652dbe208991b964bc
Abdullah <abdullahyahaya46@gmail.com>
2022-05-11 23:50:13 +0100

    Adding Authentication for Inpsectors

---
+++ controllers/inspector/authentication/index.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// importing the validation needed
+const { loginValidation } = require('../../../utils/inspector/validation');
+
+// Importing the models here
+const Inspector = require('../../../models/inspector');
+
+const login =  async(req, res) => {
+    
+    const { error } = loginValidation(req.body);
+
+    if(error) {
+        res.status(400).json({
+
+            error: "FIELD_REQUIREMENT",
+            status: false,
+            message: error.details[0].message
+
+        });
+        return;
+    }
+
+     // Checking if the user exists
+     const inspector = await Inspector.findOne({ email: req.body.email });
+
+     if(!inspector) {
+         res.status(400).json({
+             error: "NONE_ACCOUNT_EXIST",
+             status: false,
+             message: "Sorry, this account already exist"
+         });
+         return;
+     }
+
+
+      // Check if the password is correct
+    const validPass = await bcrypt.compare(req.body.password, inspector.password);
+    if(!validPass) {
+        res.status(400).json({
+            error: "INVALID_DETAILS",
+            status: false,
+            message: "Invalid Password Or Email"
+        });
+        return;
+    }
+
+     // creating and assigning token
+     jwt.sign({ id: inspector._id }, process.env.TOKEN_SECRET,  { expiresIn: '2h' }, (err, token) => {
+        
+        res.status(200).json({
+            status: true,
+            message: "Logged In",
+            token
+        })
+        return;
+        
+    });
+    
+}
+
+const resetPassword = async(req, res) => {
+    console.log("It is here now");
+}
+
+module.exports = { login, resetPassword }
--- routes/inspector/authentication/index.js
+++ routes/inspector/authentication/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+// Importing our controllers here
+const { login, resetPassword } = require('../../../controllers/inspector/authentication/index'); 
+
+router.post('/signin', login);
+
+router.post('/reset-password', resetPassword)
 
 module.exports = router;
