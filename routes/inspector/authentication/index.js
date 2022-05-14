afa93c9b6539bb9950200f7b6be2918083f32788
Abdullah <abdullahyahaya46@gmail.com>
2022-05-14 23:57:28 +0100

    Resetting Password

--- routes/inspector/authentication/index.js
+++ routes/inspector/authentication/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 // Importing our controllers here
-const { login, resetPassword } = require('../../../controllers/inspector/authentication/index'); 
+const { login, logout, resetPassword } = require('../../../controllers/inspector/authentication/index'); 
+
+// Importing the 
 
 router.post('/signin', login);
 
-router.post('/reset-password', resetPassword)
+router.get('/logout',  logout)
+
+router.patch('/reset-password', resetPassword)
 
 module.exports = router;
