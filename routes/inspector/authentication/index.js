7d189e9994c893bb0b743f6337575a4b9c44557e
Abdullah <abdullahyahaya46@gmail.com>
2022-05-17 04:10:12 +0100

    Added Routes and Updates-Profile

--- routes/inspector/authentication/index.js
+++ routes/inspector/authentication/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 // Importing our controllers here
-const { login, logout, resetPassword } = require('../../../controllers/inspector/authentication/index'); 
+const { login, logout, updateProfile, resetPassword } = require('../../../controllers/inspector/authentication/index'); 
 
-// Importing the 
+// Importing authentication for private route
+const { auth } = require('../../../middleware/inspector/verify');
 
 router.post('/signin', login);
 
 router.get('/logout',  logout)
 
 router.patch('/reset-password', resetPassword)
+
+router.put('/update-profile', auth, updateProfile)
 
 module.exports = router;
